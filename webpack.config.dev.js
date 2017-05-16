9c13d07043df67bcefbc346d93c2f58d9789aaed
vinnya3 <vincent.aceto@gmail.com>
2017-05-16 11:48:00 -0400

    feat: initial commit
    Initial Commit - include boilerplate code to initialize barebone application

---
+++ webpack.config.dev.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  context: __dirname,
+  entry: [
+    './src/js/App.js'
+  ],
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
