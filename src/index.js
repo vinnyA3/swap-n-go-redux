77d594e93433ab60963fac72b41ecf3d6c76da0f
vinnya3 <vincent.aceto@gmail.com>
2017-05-18 07:21:46 -0400

    feat: add store configuration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
+import configureStore from './store/configureStore'
+import { loadBooks } from './ducks/books'
 import routes from './routes'
+// Store
+const store = configureStore()
+store.dispatch(loadBooks())
 // Target Element
 const targetElement = document.getElementById('app')
 
 render(
-  <Router>
-    {routes}
-  </Router>,
+  <Provider store={store}>
+    <Router>{routes}</Router>
+  </Provider>,
   targetElement
 )
