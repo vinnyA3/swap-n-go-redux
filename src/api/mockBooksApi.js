a88d290843e3683e252524106cf01bcdd071dd1d
vinnya3 <vincent.aceto@gmail.com>
2017-05-18 07:20:37 -0400

    feat: fix delay import

--- src/api/mockBooksApi.js
+++ src/api/mockBooksApi.js
@@ -2,10 +2,22 @@ import delay from './delay'
 
 const Books = [
   {
-    id: '1vincent',
+    id: '1vincentimages/fnprogramming.jpg',
     title: 'Functional Programming in JS',
     imgLink: 'images/fnprogramming.jpg',
     owner: 'Vincent Aceto'
+  },
+  {
+    id: '2julieimages/javadummies.jpg',
+    title: 'Java for Dummies',
+    imgLink: 'images/javadummies.jpg',
+    owner: 'Julie Marie'
+  },
+  {
+    id: '1annieimages/cooking101.jpg',
+    title: 'Cooking 101',
+    imgLink: 'images/cooking101.jpg',
+    owner: 'Annie Marie'
   }
 ]
 
