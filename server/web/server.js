857b19b9221b76efa78eb1ced86bd7828de08085
vinnya3 <vincent.aceto@gmail.com>
2017-06-27 16:27:38 -0400

    feat: add router import

--- server/web/server.js
+++ server/web/server.js
@@ -2,11 +2,13 @@
 
 const express = require('express')
 const morgan = require('morgan')
+const router = require('./router')
 
 // init express
 const app = express()
 // set morgan logger middleware
 app.use(morgan('dev'))
+app.use(router)
 // routes
 // export
 module.exports = app
