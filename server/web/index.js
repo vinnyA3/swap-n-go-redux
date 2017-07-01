ab95992def34b22ee61b89283964207761d5f343
vinnya3 <vincent.aceto@gmail.com>
2017-06-30 23:37:48 -0400

    style: line formatting

--- server/web/index.js
+++ server/web/index.js
@@ -1,30 +1,31 @@
-'use strict'
+'use strict';
 
-const _ = require('ramda')
-const Task = require('data.task')
-const { Left, Right } = require('data.either')
-const { safeGetProp, eitherToTask } = require('../utils')
+const _ = require('ramda');
+const Task = require('data.task');
+const { Left, Right } = require('data.either');
+const { safeGetProp, eitherToTask } = require('../utils');
 // App Requires
-const http = require('http')
-const promisify = require('es6-promisify')
-const config = require('../config')
-const app = require('./server')
+const http = require('http');
+const promisify = require('es6-promisify');
+const config = require('../config');
+const app = require('./server');
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-const serverListen = promisify(server.listen, server)
+const serverListen = promisify(server.listen, server);
 
 const serverTask = port =>
   new Task((rej, res) =>
     serverListen(port)
       .then(() => res(`App Listening on port ${port}...`))
-      .catch(err => rej(`Error happened on server start: ${err}`)))
+      .catch(err => rej(`Error happened on server start: ${err}`))
+  );
 
 const startServer = config
   .map(safeGetProp('server'))
-  .chain(eitherToTask)  
+  .chain(eitherToTask)
   .map(safeGetProp('port'))
   .chain(eitherToTask)
-  .chain(serverTask)
+  .chain(serverTask);
 
-startServer.fork(console.error, console.log)
+startServer.fork(console.error, console.log);
