aaaf7ec134b95eb172829531ecbaabfd67f03ea6
vinnya3 <vincent.aceto@gmail.com>
2017-06-30 23:34:35 -0400

    feat: change ext js -> jsx

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,21 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles.scss';
 
-const Header = () => {
-  return (
-    <header className="main-header">
-      <nav className="main-header__nav">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/signin">Signin</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+const Header = () =>
+  <header className="main-header">
+    <nav className="main-header__nav">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>;
 
 export default Header;
