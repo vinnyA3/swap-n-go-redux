65da2774df58c7115a6210e3823df2efa5209b90
vinnya3 <vincent.aceto@gmail.com>
2017-07-03 02:23:15 -0400

    feat: add mongoose connection, config - temp

--- server/web/server.js
+++ server/web/server.js
@@ -1,14 +1,19 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const morgan = require('morgan')
-const router = require('./router')
-
+const express = require('express');
+const morgan = require('morgan');
+const router = require('./router');
+// temp mongoose connect - refactor
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1:27017/swap-n-go', {
+  useMongoClient: true
+});
 // init express
-const app = express()
+const app = express();
 // set morgan logger middleware
-app.use(morgan('dev'))
-app.use(router)
+app.use(morgan('dev'));
+app.use(router);
 // routes
 // export
-module.exports = app
+module.exports = app;
